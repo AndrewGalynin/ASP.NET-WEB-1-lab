8988d0e50aeb0d00fc0504dbd1d2b1c602738f63
AndrewGalynin <galynin69@gmail.com>
2024-01-19 10:56:32 +0200

    asp.net-web-1lab
    asp.net-web-1lab

---
+++ lab1/MenuController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace lab1
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MenuController : ControllerBase
+    {
+        [HttpGet("ShowWordCount/{count}")]
+        public ActionResult ShowWordCount(int count)
+        {
+            var text = System.IO.File.ReadAllText("lorem.txt");
+            var words = text.Split(new[] { ' ', '\t', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            return Ok(words.Take(count));
+        }
+
+        [HttpGet("PerformMathOperation/{operation}")]
+        public ActionResult PerformMathOperation(string operation)
+        {
+            var result = new System.Data.DataTable().Compute(operation, null);
+            return Ok(result);
+        }
+    }
+}
+
